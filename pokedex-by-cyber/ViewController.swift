c1ce0dd9152cacb2d9bd4b8107acb3a09858cffd
Barış Çöpoğlu <bariscopoglu@bariscopoglu.local>
2016-09-17 16:28:16 +0300

    Initial Commit

---
+++ pokedex-by-cyber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-cyber
+//
+//  Created by Barış Çöpoğlu on 17.09.2016.
+//  Copyright © 2016 Barış Çöpoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
